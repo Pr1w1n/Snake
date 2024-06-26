a60705b6dce45d3db2e02a16aebc5ae06d06d31d
Pr1w1n <101830291+Pr1w1n@users.noreply.github.com>
2024-06-26 22:16:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,19 @@
 const cacheName = 'Snake';
 
-const staticAssets = [ 
+const staticAssets = [
   './',
   './index.html',
   './manifest-snake.json',
-  './service-worker.js'
+  './service-worker.js',
+  './img/icon-72x72.png',
+  './img/icon-96x96.png',
+  './img/icon-128x128.png',
+  './img/icon-144x144.png',
+  './img/icon-152x152.png',
+  './img/icon-192x192.png',
+  './img/icon-256x256.png',
+  './img/icon-384x384.png',
+  './img/icon-512x512.png'
 ];
 
 self.addEventListener('install', async e => {
@@ -20,27 +29,27 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', async e => {
   const req = e.request;
   const url = new URL(req.url);
-  
-  if(url.origin === location.origin){
+
+  if (url.origin === location.origin) {
     e.respondWith(cacheFirst(req));
-  }else{
+  } else {
     e.respondWith(networkAndCache(req));
   }
 });
 
-async function cacheFirst(req){
+async function cacheFirst(req) {
   const cache = await caches.open(cacheName);
   const cached = await cache.match(req);
   return cached || fetch(req);
 }
 
-async function networkAndCache(req){
+async function networkAndCache(req) {
   const cache = await caches.open(cacheName);
-  try{
+  try {
     const fresh = await fetch(req);
     await cache.put(req, fresh.clone());
     return fresh;
-  }catch(e){
+  } catch (e) {
     const cached = await cache.match(req);
     return cached;
   }
